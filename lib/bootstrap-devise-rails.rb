50d3bb951fa7f9358d618d3da85a9ccc0c7ea00f
Derek Barnes <derek@bigbento.com>
2012-02-07 16:33:53 -0800

    Layout patch in-gem

--- lib/bootstrap-devise-rails.rb
+++ lib/bootstrap-devise-rails.rb
@@ -3,7 +3,13 @@ require "bootstrap-devise-rails/version"
 module Bootstrap
   module Devise
     module Rails
-      class Engine < ::Rails::Engine; end;
+      class Engine < ::Rails::Engine
+        initializer "less_rails_bootstrap_devise.patch_devise_controller" do
+          DeviseController.class_eval do
+            layout "devise"
+          end
+        end
+      end
     end
   end
 end
